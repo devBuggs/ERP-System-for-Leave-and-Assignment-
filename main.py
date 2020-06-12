7c50b51d3e4e6c0e82213b51a023712c2366328e
Bhagwan Singh <bhagwan.5602@gmail.com>
2020-06-12 14:20:26 +0530

    student registration accessable..

--- main.py
+++ main.py
@@ -12,22 +12,8 @@ app.secret_key = os.urandom(24)
 def main():
     return render_template('index.html')
 
-# Leave module routing functions
 @app.route("/home/leave-Approved")
 def leaveApproved():
-    '''
-    status = 'approved'
-    if request.method == 'POST':
-        db = sqlite3.connect("project.sqlite3")
-        #getcursor object it is responsible for handling database query
-        cursor = db.cursor()
-        #Execute database query
-        cursor.execute('SELECT * FROM leaveApproved WHERE status = ?', (status))
-        # Fetch one record and return result
-        account = cursor.fetchall()
-
-        db.close()
-    '''
     return ("<h1>Leave Approved Fragment!</h1>")
 
 @app.route("/home/leave-Pending")
@@ -42,26 +28,42 @@ def leaveRejected():
 def leaveApply():
     return render_template('leaveApply.html')
 
-# Assignment module routing functions
+@app.route("/leaveStatus")
+def leaveStatus():
+    return("<h1>Leave Status</h1>")
+
 @app.route("/home/assignment-Create")
 def assignmentCreate():
-    #return ("<h1>Assignment Creation Wizard!</h1>")
     return render_template('createAssignment.html')
+
+@app.route("/viewAssignment")
+def viewAssignment():
+    data=[]
+    if 'loggedin' in session:
+        user = session['id']
+        print(user)
+        # We need all the assignment info. for the user so we can display it on the view assignment page
+        db = sqlite3.connect("project.sqlite3")
+        cursor = db.cursor()
+        cursor.execute('SELECT assignment_id, assignmentinfo.name, assignmentinfo.class, assignmentinfo.faculty_id, dos, upload_date, instruction, file  FROM assignmentinfo INNER JOIN studentinfo ON studentinfo.coordinator_id=assignmentinfo.faculty_id INNER JOIN userinfo ON userinfo.faculty_id=studentinfo.coordinator_id WHERE userinfo.user_id=?',(user,))
+        data = cursor.fetchone()
+        print(data) #for testing purpose.. in CONSOLEs
+        db.close()
+        if data == None:
+            return render_template('viewAssignment.html', data=data)
+    return render_template('viewAssignment.html', data=data)
 
 @app.route("/home/assignment-Update")
 def assignmentUpdate():
-    #return ("<h1>Assignment Updation Wixzard!</h1>")
     return render_template('updateAssignment.html')
 
 @app.route("/home/assignment-Delete")
 def assignmentDelete():
-    #return ("<h1>Assignment Deletion Wizard!</h1>")
     return render_template('deleteAssignment.html')
 
-# Student Report module routing functions
 @app.route("/home/students")
 def allStudents():
-    return ("<h1>All Students Data</h1>")
+    return render_template('allStudents.html')
 
 @app.route("/home/report-Update")
 def reportUpdate():
@@ -80,20 +82,17 @@ def reportReset():
 def login():
     # Output message if something goes wrong...
     msgError = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    # Check if "email" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
         # Create variables for easy access
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         # opening database sqlite3
         db = sqlite3.connect("project.sqlite3")
-        #getcursor object it is responsible for handling database query
+        #get cursor object it is responsible for handling database query
         cursor = db.cursor()
-
         #Execute database query
-        cursor.execute('SELECT * FROM userinfo WHERE username = ? AND password = ?', (username, password,))
-        
-        ####cursor.execute('SELECT * FROM userinfo INNER JOIN studentinfo ON studentinfo.student_id=userinfo.student_id where username=? AND password=?', (username, password))
+        cursor.execute('SELECT * FROM userinfo WHERE email = ? AND password = ?', (email, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
         db.close()
@@ -101,16 +100,14 @@ def login():
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
-            #session['id'] = account['userid']
             session['id'] = account[0]
             session['username'] = account[1]
 
             #checking usertype
             if account[4] == "faculty":
-                return ("<h1>Faculty Dashboard</h1>")
-
+                render_template('facultyDashboard.html')
             elif account[4] == "student":
-                return("<h1>Student dashboard</h1>")
+                render_template('studentDashboard.html')
             # Redirect to home page
             # return 'Logged in successfully!'
             return redirect(url_for('home'))
@@ -136,31 +133,42 @@ def register():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'student_id' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        usertype = request.form['accesstype'] 
+        student_id = request.form['student_id']
+        usertype = 'student'
         # Check if account exists using MySQL
         db = sqlite3.connect("project.sqlite3")
         cursor = db.cursor()
-        #cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        cursor.execute('SELECT * FROM users WHERE username = ?', (request.form['username'],))
-        account = cursor.fetchone()
-        # If account exists show error and validation checks
-        if account:
+        # cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM studentinfo WHERE student_id = ?', (request.form['student_id'],))
+        id_found = cursor.fetchone()
+        cursor.execute('SELECT * FROM userinfo WHERE email = ?', (request.form['email'],))
+        email_found = cursor.fetchone()
+        cursor.execute('SELECT * FROM userinfo WHERE username = ?', (request.form['username'],))
+        user_found = cursor.fetchone()
+
+        # validation checks
+        if not id_found:
+            msg = 'Invalid Student ID!'
+        elif id_found and int(id_found[7]):
             msg = 'Account already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address!'
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
+        elif not username or not password or not email or not student_id:
             msg = 'Please fill out the form!'
+        elif email_found or user_found:
+            msg = 'Username/Email already exists!'
         else:
-            userid = random.randrange(999999)
+            # userid = random.randrange(999999)
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO users VALUES (?, ?, ?, ?, ?)', (userid,username, password, email, usertype))
+            cursor.execute('INSERT INTO userinfo (username, password, email, usertype, student_id) VALUES (?, ?, ?, ?, ?)', (username, password, email, usertype, student_id))
+            cursor.execute('update studentinfo set "is_register" = "1" where student_id = ?', (request.form['student_id'],))
             db.commit()
             db.close()
             msg = 'You have successfully registered!'
@@ -172,17 +180,29 @@ def register():
     return render_template('register.html', msg=msg)
 
 # http://localhost/login/home - this will be the home page, only accessible for logged-in users
-@app.route('/login/home')
+@app.route('/login/home', methods=['GET', 'POST'])
 def home():
+    account=[]
     # Check if user is loggedin
     if 'loggedin' in session:
-        # User is loggedin show them the home page
-        return render_template('home.html', username=session['username'])
+        # We need all the account info for the user so we can display it on the profile page
+        db = sqlite3.connect("project.sqlite3")
+        #getcursor object it is responsible for handling database query
+        cursor = db.cursor()
+        #Execute database query
+        cursor.execute('SELECT * FROM userinfo WHERE user_id = ?', (session['id'],))
+        account = cursor.fetchone()
+        #close the database connection
+        db.close()
+        if account[4] == "faculty":
+            return render_template('facultyDashboard.html', username=session['username'])
+        elif account[4] == "student":
+            return render_template('studentDashboard.html', username=session['username'])
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
 # http://localhost/login/profile - this will be the profile page, only accessible for logged-in users
-@app.route('/login/profile', methods = ['POST', 'GET'])
+@app.route('/login/profile', methods=['GET', 'POST'])
 def profile():
     account = []
     # Check if user is loggedin
@@ -192,11 +212,9 @@ def profile():
         #getcursor object it is responsible for handling database query
         cursor = db.cursor()
         #Execute database query
-        cursor.execute('SELECT * FROM users WHERE userid = ?', (session['id'],))
-        
+        cursor.execute('SELECT * FROM userinfo WHERE user_id = ?', (session['id'],))
         account = cursor.fetchone()
         db.commit()
-        
         #close the database connection
         db.close()
         # Show the profile page with account info
